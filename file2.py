2d2b0ac83b338faa49640f424c6c94715b610ce6
Dizzer70 <msfox3241@gmail.com>
2025-10-23 21:04:32 +0300

    Delete file2.py

--- file2.py
+++
@@ -1,4 +0,0 @@
-Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> print(47, 52, 150, sep='  ')
-47  52  150
